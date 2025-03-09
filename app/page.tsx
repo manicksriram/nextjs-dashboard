0fac580a674df20b26feeced72774f399bfc115a
manicksriram <manicksriram222@gmail.com>
2025-03-09 13:07:56 +0530

    page.tsx file changes done

--- app/page.tsx
+++ app/page.tsx
@@ -16,10 +16,10 @@ export default function Page() {
 <p
       className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
     >            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
+            <Link href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+            </Link>
+          
           </p>
           <Link
             href="/login"
