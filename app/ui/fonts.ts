89d7b87e22b7a16e1a4585f4bd36073104834cc1
manick <manicksriram222@gmail.com>
2025-03-09 12:32:58 +0530

    next chapter 6

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter,Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin','cyrillic'] });
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
